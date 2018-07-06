1326e37be83018b0968994f34ae102be9a8facf7
MartyEwings <martyewings@gmail.com>
2018-07-06 11:30:11 +0100

    changed single quotes
    added in standard start and end to match

--- tasks/kb0244.sh
+++ tasks/kb0244.sh
@@ -2,5 +2,8 @@
 
 # Puppet Task Name: KB0244
 #
+echo " -KB#0244 Task started   $(date +%s)  --"
 
-puppet apply -e "if ($::facts['kernel'] == "linux") {exec {'Remove MCollective logrotate configuration': command => '/bin/rm /etc/logrotate.d/mcollective',onlyif  => '/bin/test -s /etc/logrotate.d/mcollective',}}"
+puppet apply -e "if ($::facts['kernel'] == 'linux') {exec {'Remove MCollective logrotate configuration': command => '/bin/rm /etc/logrotate.d/mcollective',onlyif  => '/bin/test -s /etc/logrotate.d/mcollective',}}"
+
+echo " -KB#0244 Task ended   $(date +%s)    --"
