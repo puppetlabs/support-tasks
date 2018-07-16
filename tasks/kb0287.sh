951afe367715d7e02c1fe636f4903f9fe5e4c0e6
Daniel Mcilhagga <danny.mcilhagga@danny.mcilhagga-C02T60P2G8WL>
2018-07-16 09:20:51 +0100

    Adding kb0287.json and kb0287.sh

---
+++ tasks/kb0287.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Puppet Task Name: KB0287
+
+declare PT_dbname
+dbname=$PT_dbname
+
+if [ "puppet resource service pe-postgresql | grep running" ]
+then
+echo "pe-postgresql service detected, will continue to run."
+
+su - pe-postgres -s /bin/bash -c "/opt/puppetlabs/server/bin/psql -d $dbname -c '\di+;'"
+
+else
+  echo "Node not running pe-postgresql service, please select node which is."
+
+fi
+ echo " -- KB#0287 Task ended: $(date +%s) --"
