352cdc42a6431fdf7dcc7beb532ebcf61fd3560b
daniel5119 <dp20@hotmail.co.uk>
2018-07-17 15:49:30 +0100

    Adding ability to select all tables from all dbs

--- tasks/kb0287_Check_DB_Table_Sizes.sh
+++ tasks/kb0287_Check_DB_Table_Sizes.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-# Puppet Task Name: kb0287_Check_DB_Table_Sizes
+# Puppet Task Name: KB0287_Check_DB_Table_Sizes
 
 declare PT_dbname
 dbname=$PT_dbname
 
 if puppet resource service pe-postgresql | grep -q running
 then
-  echo "pe-postgresql service detected, will continue to run."
-  su - pe-postgres -s /bin/bash -c "/opt/puppetlabs/server/bin/psql -d $dbname -c '\di+;'"
+  if $dbname != 'all'
+  then
+    echo "pe-postgresql service detected, will continue to run."
+    su - pe-postgres -s /bin/bash -c "/opt/puppetlabs/server/bin/psql -d $dbname -c '\di+;'"
+  else
+    echo "pe-postgresql service detected, will continue to run."
+    dbnames='pe-puppetdb pe-postgres pe-classifier pe-rbac pe-activity pe-orchestrator postgres'
+    for dbnames in $dbnames
+    do
+      su - pe-postgres -s /bin/bash -c "/opt/puppetlabs/server/bin/psql -d $dbname -c '\di+;'"
+    done
+  fi
 else
   echo "Node not running pe-postgresql service, please select node which is."
 fi
