92c7e9ac6fe41713090051358a45777fcee0bec5
Jarret Lavallee <jarretlavallee@users.noreply.github.com>
2018-08-10 16:35:37 -0700

    (maint) Standardize output messages in 236 and 267 (#22)
    Prior to this commit there were some inconsistencies in the output from
    these tasks. This commit standardizes them.

--- tasks/kb0267_clear_file_sync_locks.sh
+++ tasks/kb0267_clear_file_sync_locks.sh
@@ -3,7 +3,7 @@
 
 if [ -e "/etc/sysconfig/pe-puppetserver" ] || [ -e "/etc/default/pe-puppetserver" ] # Test to confirm this is a Puppetserver
 then
-  echo "-Puppetmaster node detected"   #Log Line to StdOut for the Console
+  echo "Puppet master node detected"   #Log Line to StdOut for the Console
 
   echo "  Ensuring the Puppetserver service is stopped"
   puppet resource service pe-puppetserver ensure=stopped
@@ -12,7 +12,7 @@ then
   echo "  Ensuring the Puppetserver service is running"
   puppet resource service pe-puppetserver ensure=running
 else
-  echo  "-Not a Puppet MASTER node exiting"
+  echo  "Not a Puppet master node, exiting"
 
 fi
-echo " -KB#0267 Task ended   $(date +%s)    --"
+echo "KB#0267 Task ended   $(date +%s)    --"
