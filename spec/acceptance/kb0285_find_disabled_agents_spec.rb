194559242002071d95c4ef566463258e3470f6e2
Martin Ewings <martin.ewings@martin.ewings-C02T23THG8WL>
2019-04-29 16:06:12 +0100

    updated run_task to run_bolt_task for litmus 0.1.1

--- spec/acceptance/kb0285_find_disabled_agents_spec.rb
+++ spec/acceptance/kb0285_find_disabled_agents_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper_acceptance'
 describe 'tasks' do
   it 'when agent is disabled ' do
     run_shell('/opt/puppetlabs/bin/puppet agent --disable')
-    results = task_run('support_tasks::kb0285_find_disabled_agents')
+    results = run_bolt_task('support_tasks::kb0285_find_disabled_agents')
     expect(results.first['result']['_output']).to contain('Puppet agent is disabled')
   end
   it 'when agent is enabled ' do
     run_shell('/opt/puppetlabs/bin/puppet agent --enable')
-    expect { task_run('support_tasks::kb0285_find_disabled_agents') }.to raise_error do |error|
+    expect { run_bolt_task('support_tasks::kb0285_find_disabled_agents') }.to raise_error do |error|
       expect(error).to be_a(RuntimeError)
       expect(error.message).to match %r{"_output"=>"Puppet agent is enabled\\n"}
       expect(error.message).to match %r{"exit_code"=>1}
