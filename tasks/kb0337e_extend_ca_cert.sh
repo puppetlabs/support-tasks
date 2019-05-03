c12e5180496fec3172b80dbe2c89c9cc8ead77b7
Adrian Parreiras Horta <adrian.parreiras-horta@puppet.com>
2019-05-03 12:37:57 -0700

    Move extend.sh to its own dir under files/
    Also fix unnecessary dependency in configure_master.json

--- tasks/kb0337e_extend_ca_cert.sh
+++ tasks/kb0337e_extend_ca_cert.sh
@@ -7,7 +7,7 @@ source "$PT__installdir/support_tasks/files/common.sh"
 
 echo "test" | base64 -w 0 - &>/dev/null || fail "base64_test" "This utility requires a version of base64 with the -w flag"
 
-new_cert="$(bash "$PT__installdir/support_tasks/files/extend.sh")" || fail "extend_cert"
+new_cert="$(bash "$PT__installdir/support_tasks/files/kb0337/extend.sh")" || fail "extend_cert"
 contents="$(base64 -w 0 "$new_cert")" || fail "encode_cert"
 
 success "{ \"status\": \"success\", \"new_cert\": \"$new_cert\", \"contents\": \"$contents\" }"
