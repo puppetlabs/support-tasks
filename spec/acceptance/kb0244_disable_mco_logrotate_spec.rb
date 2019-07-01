ead693d5c5e10d8a262979108bb8e98dd015a6dd
Martin Ewings <martin.ewings@martin.ewings-C02T23THG8WL>
2019-07-01 15:04:16 +0100

    litmus updates

--- spec/acceptance/kb0244_disable_mco_logrotate_spec.rb
+++ spec/acceptance/kb0244_disable_mco_logrotate_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper_acceptance'
 describe 'tasks' do
   it 'when mco logrotate is present ' do
     run_shell('touch /etc/logrotate.d/mcollective')
-    results = run_bolt_task('support_tasks::kb0244_disable_mco_logrotate')
-    expect(results.first['result']['_output']).to contain('Applied catalog in')
+    result = run_bolt_task('support_tasks::kb0244_disable_mco_logrotate')
+    expect(result['result']['_output']).to contain('Applied catalog in')
     expect(File).not_to exist('/etc/logrotate.d/mcollective')
   end
   it 'when mco logrotate is not present ' do
     run_shell('rm -rf /etc/logrotate.d/mcollective')
-    results = run_bolt_task('support_tasks::kb0244_disable_mco_logrotate')
-    expect(results.first['result']['_output']).to contain('Applied catalog in')
+    result = run_bolt_task('support_tasks::kb0244_disable_mco_logrotate')
+    expect(result['result']['_output']).to contain('Applied catalog in')
     expect(File).not_to exist('/etc/logrotate.d/mcollective')
   end
 end
--- spec/acceptance/kb0346a_bash_herd_resolver_spec.rb
+++ spec/acceptance/kb0346a_bash_herd_resolver_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper_acceptance'
 describe 'tasks' do
   it 'when restarting agent ' do
     run_shell('/opt/puppetlabs/bin/puppet config set runinterval 5')
-    results = run_bolt_task('support_tasks::kb0346a_bash_herd_resolver')
-    expect(results.first['result']['_output']).to contain('running')
-    expect(results.first['result']['_output']).to contain('stopped')
+    result = run_bolt_task('support_tasks::kb0346a_bash_herd_resolver')
+    expect(result['result']['_output']).to contain('running')
+    expect(result['result']['_output']).to contain('stopped')
   end
 end
