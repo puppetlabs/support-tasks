578537ef64bdeead3903be3801773d15e3a368f6
Martin Ewings <martin.ewings@martin.ewings-C02T23THG8WL>
2019-08-27 19:54:10 +0100

    fix ruby syntax

--- spec/acceptance/kb0236_set_cache_paths_to_default_spec.rb
+++ spec/acceptance/kb0236_set_cache_paths_to_default_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper_acceptance'
 describe 'tasks' do
+  it 'when all dirs are already default ' do
+    result = run_bolt_task('support_tasks::kb0236_set_cache_paths_to_default')
+    expect(result['result']['_output']).to contain('No changes necessary')
+  end
   it 'when vardir is not default ' do
     run_shell('/opt/puppetlabs/bin/puppet config set vardir /tmp')
     result = run_bolt_task('support_tasks::kb0236_set_cache_paths_to_default')
@@ -14,9 +18,5 @@ describe 'tasks' do
     run_shell('/opt/puppetlabs/bin/puppet config set rundir /tmp')
     result = run_bolt_task('support_tasks::kb0236_set_cache_paths_to_default')
     expect(result['result']['_output']).to contain('rundir set to /tmp, resetting to the default')
-  end
-  it 'when all dirs are already default ' do
-    result = run_bolt_task('support_tasks::kb0236_set_cache_paths_to_default')
-    expect(result['result']['_output']).to contain('No changes necessary')
   end
 end
