7cd84a404d33af582fae01dd36e58e9bab8125b6
Patrick Grant <32844876+pgrant87@users.noreply.github.com>
2019-12-09 16:59:31 +0000

    [SUP-1531] (#61)
    * Open source additions
    
    * Update kb0236_set_cache_paths_to_default.sh
    
    * OSP additions
    
    * a few wording changes

--- tasks/kb0149_resolve_stack_level_too_deep.sh
+++ tasks/kb0149_resolve_stack_level_too_deep.sh
@@ -24,8 +24,30 @@ then
                 puppet resource service pe-puppetserver ensure=stopped
                 puppet resource service pe-puppetserver ensure=running
         fi
+elif [ -e "/etc/sysconfig/puppetserver" ] # cover OSP EL-based system
+then
+ echo "-Puppetserver node detected - EL "   #Log Line to StdOut for the Console
+
+
+        if $(/usr/bin/which grep) -q "Xss" /etc/sysconfig/puppetserver
+        then  echo "Argument Already  Present"
+        else  $(/usr/bin/which sed) -i 's/^\s*\(JAVA_ARGS="\)/JAVA_ARGS="-Xss2m /g' /etc/sysconfig/puppetserver
+                puppet resource service puppetserver ensure=stopped
+                puppet resource service puppetserver ensure=running
+        fi
+elif [ -e "/etc/default/puppetserver" ] # cover ubuntu OSP
+then
+ echo "-Puppetserver node detected - Ubuntu "   #Log Line to StdOut for the Console
+
+
+        if $(/usr/bin/which grep) -q "Xss" /etc/default/puppetserver
+        then  echo "Argument Already  Present"
+        else  $(/usr/bin/which sed) -i 's/^\s*\(JAVA_ARGS="\)/JAVA_ARGS="-Xss2m /g' /etc/default/puppetserver
+                puppet resource service puppetserver ensure=stopped
+                puppet resource service puppetserver ensure=running
+        fi
 else
-  echo  "-Not a Puppet MASTER node exiting "
+  echo  "-Not a Puppetserver node exiting "
 
 fi
 echo " -KB#0149 Task ended   $(date +%s)    --"
