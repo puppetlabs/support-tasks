9d8280821a98f466460a7e8f0771254d82fe42a9
martyewings <martyewings@gmail.com>
2019-12-10 12:09:18 +0000

    SC2059

--- tasks/kb0307_generate_token.sh
+++ tasks/kb0307_generate_token.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Generate Token for requested user, for consumption in other tasks
-
+# shellcheck disable=SC2086
 declare PT_user
 declare PT_password
 password=$PT_password
 user=$PT_user
 
 
-printf "$password" | puppet access login $user --lifetime 1d
+printf "%s" "$password" | puppet access login $user --lifetime 1d
