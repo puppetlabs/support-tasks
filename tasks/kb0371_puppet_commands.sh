2b92098f7e8a51b1a8f88ac9d003e4bb5e67cbd1
MartyEwings <martyewings@gmail.com>
2019-12-10 20:47:46 +0000

    Console Command Line Tasks [SUP-1383] (#62)
    * Update CHANGELOG.md
    
    * Update README.md
    
    * os command task
    
    * Fixed broken case statement
    
    * OS commands
    
    * api calls and token generate
    
    * fixing metadata issues
    
    * Update kbxxx_generate_token.sh
    
    * kb article numbering
    
    * SC2046
    
    * SC2046
    
    * SC2059
    
    * SC2059

---
+++ tasks/kb0371_puppet_commands.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#
+
+
+declare PT_command
+command=$PT_command
+
+
+
+if [ -e "/etc/sysconfig/pe-puppetserver" ] || [ -e "/etc/default/pe-puppetserver" ] # Test to confirm this is a Puppetserver
+then
+  echo "Puppet master node detected"   #Log Line to StdOut for the Console
+
+
+case $command in
+     config_print)
+          puppet config print
+          ;;
+       module_list)
+           puppet module list --all
+          ;;
+     infrastructure_status)
+          puppet infrastructure status
+          ;;
+     tune)
+          puppet infrastructure tune
+          ;;
+esac
+else
+  echo  "Not a Puppet master node, exiting"
+
+fi
---
+++ tasks/kb0372_os_commands.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#
+
+
+declare PT_command
+command=$PT_command
+
+
+
+if [ -e "/etc/sysconfig/pe-puppetserver" ] || [ -e "/etc/default/pe-puppetserver" ] # Test to confirm this is a Puppetserver
+then
+  echo "Puppet master node detected"   #Log Line to StdOut for the Console
+
+
+case $command in
+     puppet_port_status)
+          netstat -ln | grep '8140\|5432\|8170\|8143\|443 \|4433\|8081\|8150\|8151'
+          ;;
+       puppetserver_log)
+           tail -100 /var/log/puppetlabs/puppetserver/puppetserver.log
+          ;;
+  puppetdb_log)
+          tail -100 /var/log/puppetlabs/puppetdb/puppetdb.log
+          ;;
+     console_log)
+          tail -100 /var/log/puppetlabs/console-services/console-services.log
+          ;;
+    orchestrator_log)
+          tail -100 /var/log/puppetlabs/orchestration-services/orchestration-services.log
+          ;;
+  syslog_log)
+	  if [ -e "/var/log/messages" ]
+	    then
+                tail -100 /var/log/messages
+	  elif [ -e "/var/log/syslog" ]
+            then
+               tail -100 /var/log/syslog
+	else 
+		echo "No default syslog found"
+	fi
+          ;;
+  ssldir_permissions)
+          find "$(puppet config print ssldir)" -maxdepth 10 -type d -exec ls -ld "{}" \;
+          ;;
+esac
+else
+  echo  "Not a Puppet master node, exiting"
+
+fi
+
+
+
