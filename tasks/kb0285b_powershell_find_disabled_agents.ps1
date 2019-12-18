44957de55d7d79fcf6830a8816a7890b171f7f79
Chris Webster <50930031+cwebster61083@users.noreply.github.com>
2019-12-18 14:38:38 -0800

    Add windows find disabled agents ps1 (#66)
    * Add Windows Support kb0285
    
    Added kb0285_find_disabled_agents.ps1 to enable detection of disabled
    agents on Windows.
    
    Modified kb0285_find_disabled_agents.json so it will use the correct
    version of the script on Linux and Windows.
    
    * Updated kb0285_find_disabled_agents.ps1
    
    Updated it so that it functions like the Linux version.
    
    * Added blank line to the end of file.
    
    * Updated kb0285 tasks
    
    Updated kb0285 for backwards compatability.

---
+++ tasks/kb0285b_powershell_find_disabled_agents.ps1
@@ -0,0 +1,10 @@
+$LOCKFILE="$(cmd.exe /c puppet config print statedir)/agent_disabled.lock"
+
+if(Test-Path $LOCKFILE) {
+  Write-Output "Puppet agent is disabled"
+  cat $LOCKFILE
+}
+else {
+  Write-Output "Puppet agent is enabled"
+  EXIT 1
+}
