596bd354a6bd46540d092ce20302bfcfa5bcf698
Patrick Grant <32844876+pgrant87@users.noreply.github.com>
2020-01-28 15:48:52 +0000

    (SUP-1533)
    OSP compatibility changes for his db table task

--- tasks/st0287_check_db_table_sizes.sh
+++ tasks/st0287_check_db_table_sizes.sh
@@ -4,21 +4,53 @@
 declare PT_dbname
 dbname=$PT_dbname
 
-if puppet resource service pe-postgresql | grep -q running
-then
-  if [ "$dbname" != 'all' ]
-  then
-    echo "pe-postgresql service detected, will continue to run."
-    su - pe-postgres -s /bin/bash -c "/opt/puppetlabs/server/bin/psql -d $dbname -c '\di+;'"
+function getdbTables() {
+  path=$1
+  dbname=$2
+  service=$3
+  shift 3
+  dblist=("$@")
+  if [ "$dbname" != 'all' ]; then
+    echo "${service} service detected, will continue to run against ${dbname}."
+    su - $service -s /bin/bash -c "$path -d $dbname -c '\di+;'"
   else
-    echo "pe-postgresql service detected, will continue to run."
-    pedbnames='pe-puppetdb pe-postgres pe-classifier pe-rbac pe-activity pe-orchestrator postgres'
-    for pedbnames in $pedbnames
+    echo "${service} service detected, will continue to run against ${dblist}."
+    for db in $dblist
     do
-      su - pe-postgres -s /bin/bash -c "/opt/puppetlabs/server/bin/psql -d $pedbnames -c '\di+;'"
+      su - $service -s /bin/bash -c "$path -d $db -c '\di+;'"
     done
   fi
+}
+
+#Determine if PE or OSP Postgres
+if puppet resource service pe-postgresql | grep -q running; then
+  postgresservice="pe-postgres"
+  if [ -z "$dbname" ]; then
+    dbname="pe-puppetdb"
+  fi
+elif puppet resource service postgresql-* | grep -q running; then
+  postgresservice="postgres"
+  if [ -z "$dbname" ]; then
+    dbname="puppetdb"
+  fi
 else
-  echo "Node not running pe-postgresql service, please select node which is."
+  echo "Node not running pe-postgresql or postgresql service, please select node which is."
 fi
-  echo " -- ST#0287 Task ended: $(date +%s) --"
+
+#Run for the correct environment
+  case "${postgresservice}" in
+    pe-postgres)
+      echo "Found PE-Postgres"
+      pedbnames='pe-puppetdb pe-postgres pe-classifier pe-rbac pe-activity pe-orchestrator'
+      getdbTables "/opt/puppetlabs/server/bin/psql" "${dbname}" "${postgresservice}" "${pedbnames}"
+      ;;
+    postgres)
+      echo "Found Postgres"
+      getdbTables "psql" "${dbname}" "${postgresservice}" "puppetdb"
+      ;;
+    *)
+      echo "Cannot Determine if Puppet Enterprise or Puppet Open Source "
+      exit 1
+  esac
+
+echo " -- ST#0287 Task ended: $(date +%s) --"
