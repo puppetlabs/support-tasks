2d417c64e815de2eb25645e992e57ed90b562a25
elainemccloskey <44435892+elainemccloskey@users.noreply.github.com>
2020-02-13 14:19:26 +0000

    Update st0346a_bash_herd_resolver.sh
    Added full path to puppet

--- tasks/st0346a_bash_herd_resolver.sh
+++ tasks/st0346a_bash_herd_resolver.sh
@@ -4,6 +4,6 @@
 # This task is to be run on operating systems using a bash shell
 # It will randomise the restart of the puppet agent based on the current value of runinterval
 
-sleep $(( ( RANDOM % $(puppet agent --configprint runinterval) )  + 1 ))s
-puppet resource service puppet ensure=stopped
-puppet resource service puppet ensure=running
+sleep $(( ( RANDOM % $(/opt/puppetlabs/bin/puppet agent --configprint runinterval) )  + 1 ))s
+/opt/puppetlabs/bin/puppet resource service puppet ensure=stopped
+/opt/puppetlabs/bin/puppet resource service puppet ensure=running
