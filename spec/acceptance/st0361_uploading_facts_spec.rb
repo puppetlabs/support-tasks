35e5a84400579b21d62284fcb7724b0172dc27aa
martyewings <martyewings@gmail.com>
2021-12-10 14:28:31 +0000

    (SUP-2848) st0361a_uploading_facts convert to ruby

--- spec/acceptance/st0361_uploading_facts_spec.rb
+++ spec/acceptance/st0361_uploading_facts_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper_acceptance'
 describe 'tasks' do
   it 'facts uploaded successfully' do
     result = run_bolt_task('support_tasks::st0361_uploading_facts')
-    expect(result['result']['STDOUT']).to contain('Uploading facts for')
+    expect(result['result']).to contain('Uploading facts for')
   end
 end
