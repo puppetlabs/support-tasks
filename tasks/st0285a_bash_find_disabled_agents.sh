98f6795bade55b86ef7d05c886c9d54a2135fa52
Aaron McCracken <aaron.mccracken@perforce.com>
2023-11-02 14:51:34 +0000

    Rearrange

--- tasks/st0285a_bash_find_disabled_agents.sh
+++ tasks/st0285a_bash_find_disabled_agents.sh
@@ -3,8 +3,8 @@
 # DEPRECATION:
 # This script is now Deprecated and will be removed in a further update
 
-LOCKFILE="$(puppet config print statedir)/agent_disabled.lock"
 task-output "deprecation" "This task is deprecated and will be removed in a future release. Please see this module's README for more information"
+LOCKFILE="$(puppet config print statedir)/agent_disabled.lock"
 
 if [ -e "$LOCKFILE" ] 
 then
