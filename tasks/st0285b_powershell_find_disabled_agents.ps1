4c787cbc145180fa6e664c0b29809524e10c2f28
Aaron McCracken <aaron.mccracken@perforce.com>
2023-11-08 13:42:01 +0000

    SUP-4593_Deprecation_st0285_and_readme

--- tasks/st0285b_powershell_find_disabled_agents.ps1
+++ tasks/st0285b_powershell_find_disabled_agents.ps1
@@ -1,5 +1,6 @@
 $LOCKFILE="$(cmd.exe /c puppet config print statedir)/agent_disabled.lock"
 
+Write-Output "This task is deprecated and will be removed in a future release. Please see this module's README for more information"
 if(Test-Path $LOCKFILE) {
   Write-Output "Puppet agent is disabled"
   cat $LOCKFILE
