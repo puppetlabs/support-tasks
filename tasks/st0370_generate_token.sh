3d857017118aaaec4d6b1ea7dd516ba5cb024a6f
Aaron McCracken <aaron.mccracken@perforce.com>
2023-11-09 11:57:01 +0000

    SUP-4600 Deprecation of Generate_Token Task with Readme Update

--- tasks/st0370_generate_token.sh
+++ tasks/st0370_generate_token.sh
@@ -2,8 +2,13 @@
 
 # Generate Token for requested user, for consumption in other tasks
 # shellcheck disable=SC2086
+
+# DEPRECATION:
+# This script is now Deprecated and will be removed in a further update
 declare PT_user
 declare PT_password
+
+echo "This task is deprecated and will be removed in a future release. Please see this module's README for more information"
 password=$PT_password
 user=$PT_user
 
